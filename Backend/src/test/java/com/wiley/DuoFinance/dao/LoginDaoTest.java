90d6eee052eb7a2dbb7876274d2f272717fb3226
Danikh25 <dani.kha@hotmail.com>
2024-04-12 17:06:48 -0400

    Added tests for login and creating a user

--- Backend/src/test/java/com/wiley/DuoFinance/dao/LoginDaoTest.java
+++ Backend/src/test/java/com/wiley/DuoFinance/dao/LoginDaoTest.java
@@ -1,7 +1,6 @@
 package com.wiley.DuoFinance.dao;
 
 import com.wiley.DuoFinance.TestApplicationConfiguration;
-import com.wiley.DuoFinance.exception.InvalidCredentialsException;
 import com.wiley.DuoFinance.model.Credentials;
 import com.wiley.DuoFinance.model.User;
 import org.junit.jupiter.api.AfterEach;
@@ -14,8 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = TestApplicationConfiguration.class)
@@ -30,26 +28,51 @@ public class LoginDaoTest {
 
     @BeforeEach
     public void setUp() {
-        jdbc.update("INSERT INTO user (username, email, password) VALUES (?, ?, ?)", "testuser", "test@email.com", "password");
+
     }
 
     @AfterEach
     public void tearDown() {
-        jdbc.update("DELETE FROM user");
+
     }
 
     @Test
     @DisplayName("Test correct credentials")
-    public void testLoginSuccess() throws InvalidCredentialsException {
-        Credentials credentials = new Credentials("test@email.com", "password");
+    public void testLoginSuccess() {
+        Credentials credentials = new Credentials("admin@gmail.com", "password");
         User actualUser = loginDao.login(credentials);
-        assertEquals(credentials.getEmail(), actualUser.getEmail());
+        assertEquals(credentials.getEmail(), actualUser.getEmail(),"Email should match in the database");
     }
 
     @Test
     @DisplayName("Test wrong credentials")
     public void testLoginFailure() {
         Credentials credentials = new Credentials("wrong@email.com", "wrongpassword");
-        assertThrows(InvalidCredentialsException.class, () -> loginDao.login(credentials));
+        User actualUser = loginDao.login(credentials);
+        assertNull(actualUser, "User should be null since the records does not exist in the database");
+    }
+
+    @Test
+    @DisplayName("Test if basic user")
+    public void testConfirmBasicStatus() {
+        int basicUserId = 2; // userId = 2 -> user (from our database)
+        boolean isBasicUser = loginDao.confirmBasicStatus(basicUserId);
+        assertTrue(isBasicUser, "User should have basic status");
+
+        int nonRoleId = 1; // userId that does not match basic
+        boolean isNonBasicUser = loginDao.confirmBasicStatus(nonRoleId);
+        assertFalse(isNonBasicUser, "User should not have basic status");
+    }
+
+    @Test
+    @DisplayName("Test if admin user")
+    public void testConfirmAdminStatus() {
+        int adminUserId = 1; // userId = 1 -> admin (from our database)
+        boolean isAdminUser = loginDao.confirmAdminStatus(adminUserId);
+        assertTrue(isAdminUser, "User should have admin status");
+
+        int nonRoleId = 2; // userId that does not match admin
+        boolean isNonAdminUser = loginDao.confirmAdminStatus(nonRoleId);
+        assertFalse(isNonAdminUser, "User should not have admin status");
     }
 }
--- Backend/src/test/java/com/wiley/DuoFinance/dao/UserDaoTest.java
+++ Backend/src/test/java/com/wiley/DuoFinance/dao/UserDaoTest.java
@@ -1,11 +1,19 @@
 package com.wiley.DuoFinance.dao;
 
 import com.wiley.DuoFinance.TestApplicationConfiguration;
+import com.wiley.DuoFinance.model.Role;
+import com.wiley.DuoFinance.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = TestApplicationConfiguration.class)
@@ -13,8 +21,80 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class UserDaoTest {
 
     @Autowired
-    private UserDaoTest userDao;
+    private UserMySqlDao userDao;
 
     @Autowired
     private JdbcTemplate jdbc;
+
+    private int testUserId;
+    @BeforeEach
+    public void setUp() {
+        // Insert a test user before each test
+        User testUser = new User();
+        testUser.setUsername("testuser");
+        testUser.setEmail("testuser@gmail.com");
+        testUser.setPassword("password");
+        testUserId = userDao.addUser(testUser);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Delete the test user after each test
+        userDao.deleteUserById(testUserId);
+    }
+    @Test
+    @DisplayName("Test adding a user")
+    public void testAddUser(){
+        //Creating a user to be added
+        User user = new User();
+        user.setUsername("test1");
+        user.setEmail("test1@email.com");
+        user.setPassword("password");
+        int userId = userDao.addUser(user);
+        //If userId positive it means that it was created
+        assertTrue(userId > 0, "userId should be positive");
+
+        User insertedUser = userDao.getUserById(userId);
+        assertEquals(user.getUsername(), insertedUser.getUsername(), "Inserted user's username should match the original");
+        assertEquals(user.getEmail(), insertedUser.getEmail(), "Inserted user's email should match the original");
+        userDao.deleteUserById(userId); //deleting it so it doesnt save in our database
+    }
+    @Test
+    @DisplayName("Test deleting a user by ID")
+    public void testDeleteUserById() {
+        User user = new User();
+        user.setUsername("test2");
+        user.setEmail("test2@email.com");
+        user.setPassword("password");
+        int userId = userDao.addUser(user);
+
+        userDao.deleteUserById(userId);
+
+        User deletedUser = userDao.getUserById(userId);
+        assertNull(deletedUser, "User should be null after deletion");
+    }
+    @Test
+    @DisplayName("Test checking email availability")
+    public void testIsEmailAvailable() {
+        String existingEmail = "admin@gmail.com"; //Available in our database
+        boolean isAvailable = userDao.isEmailAvailable(existingEmail);
+        assertFalse(isAvailable, "Email should be already taken");
+
+        String nonExistingEmail = "nonexistent@email.com"; //non existent is our database
+        boolean notAvailable = userDao.isEmailAvailable(nonExistingEmail);
+        assertTrue(notAvailable, "Email should be available to create");
+    }
+
+    @Test
+    @DisplayName("Test getting a user by ID")
+    public void testGetUserById() {
+        User user = userDao.getUserById(testUserId);
+        assertNotNull(user, "User should not be null");
+
+        int nonExistentUserId = 9999; // ID not available in our database
+        user = userDao.getUserById(nonExistentUserId);
+        assertNull(user, "User should be null");
+    }
+
 }
+
