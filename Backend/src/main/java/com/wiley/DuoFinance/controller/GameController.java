943a7a58d4352347db09a4013162ff7643bb5b29
alxeandre <alexandre.mr.billerau@gmail.com>
2024-04-15 14:28:42 -0400

    add the credential cors

--- Backend/src/main/java/com/wiley/DuoFinance/controller/GameController.java
+++ Backend/src/main/java/com/wiley/DuoFinance/controller/GameController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.POST, RequestMethod.GET}, allowCredentials = "true")
 public class GameController {
 
     @Autowired
